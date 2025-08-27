b3dd2b86618fd78aca915dbd3b2350df094baee7
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-08-27 08:30:00 +0200

    Java Practise - Strings (String escape characters)

--- src/Strings/StringLiterals.java
+++ src/Strings/StringLiterals.java
@@ -4,10 +4,19 @@ public class StringLiterals {
 
     static void main() {
         stringLiteral();
+        stringEscapeCharacters();
     }
 
     public static void stringLiteral() {
         String myString = new String("Hello World");
         System.out.println("My String " + myString);
+    }
+
+    public static void stringEscapeCharacters () {
+        System.out.println(" Translated into a single \\ character in the String");
+        System.out.println(" Translated into a single \ttab character in the string");
+        System.out.println(" Translated into a single \r carriage return character in the string");
+        System.out.println(" Translated into a single \n new line character in the string");
+
     }
 }
