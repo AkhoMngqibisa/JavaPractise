4aa7f30ede05d986941f99c79d1fd09280f2c4b0
AkhoMngqibisa <akhomngqibisa@gmail.com>
2025-08-27 08:15:00 +0200

    Java Practise - Strings (String literal)

---
+++ src/Strings/StringLiterals.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class StringLiterals {
+
+    static void main() {
+        stringLiteral();
+    }
+
+    public static void stringLiteral() {
+        String myString = new String("Hello World");
+        System.out.println("My String " + myString);
+    }
+}
